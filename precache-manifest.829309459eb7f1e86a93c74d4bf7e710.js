4599491b966c5b8c68257e51eeb5ce0bbc77b8c8
H <quanhungbang@gmail.com>
2018-11-22 09:56:47 +0700

    init project

---
+++ precache-manifest.829309459eb7f1e86a93c74d4bf7e710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "dd1132879030ffc1ffca",
+    "url": "js/chunk-f57f6914.848761b1.js"
+  },
+  {
+    "revision": "c208a64ad138c76be5a9",
+    "url": "js/chunk-2d22d746.642491df.js"
+  },
+  {
+    "revision": "638cd2a75daef0482394",
+    "url": "js/app.7bf3d741.js"
+  },
+  {
+    "revision": "01e669bc716a097be9bec710522ae653",
+    "url": "index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "638cd2a75daef0482394",
+    "url": "css/app.af072f6e.css"
+  }
+];
